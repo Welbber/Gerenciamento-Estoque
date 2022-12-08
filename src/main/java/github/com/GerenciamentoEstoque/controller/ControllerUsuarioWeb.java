304eb45a962fde72404125963ddcdd8bfe1ea915
welbber <welbervital01@gmail.com>
2022-12-08 20:48:20 -0300

    adicionando nova estrutura no projeto

---
+++ src/main/java/github/com/GerenciamentoEstoque/controller/ControllerUsuarioWeb.java
@@ -0,0 +1,7 @@
+package github.com.GerenciamentoEstoque.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ControllerUsuarioWeb {
+}
---
+++ src/test/java/github/com/GerenciamentoEstoque/GerenciamentoEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.GerenciamentoEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
